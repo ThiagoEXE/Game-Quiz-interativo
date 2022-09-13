d016ae4e88bddde9707b7b741d79d6ff769b80d0
Kelliton Santana De Souza <programmerKelliton@gmail.com>
2022-09-12 21:40:11 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/quiz/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static String questão[] ={
+
+            "Which company owns the amdroid?",
+            "Which one is the  progamming language?",
+            "Where you are watching this video?"
+    };
+
+    public static String escolha[][] = {
+
+            {"Google","Apple","Nokia"},
+            {"Java","Kotlin","Notepad"},
+            {"Facebook","Instagram","Youtube"}
+    };
+    public static String escolhaCerta[] ={
+            "Google",
+            "Notepad",
+            "Youtube"
+    };
+}
